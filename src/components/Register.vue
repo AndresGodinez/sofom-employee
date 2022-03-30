0a1b913829b4671b4f160eeb68ba6ff17368d8ab
AGo <ing.a.godinez@gmail.com>
2022-03-30 16:11:12 -0600

    feat header-top was added

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,16 +1,23 @@
 <template>
-  <v-container>
-    <v-row class="full" style="height: 100vh">
-      <v-col sm="12" cols="12" align-self="center">
-        <h1>Registro</h1>
-      </v-col>
-    </v-row>
-  </v-container>
+  <div>
+    <HeaderTop></HeaderTop>
+    <v-container>
+      <v-row class="full" style="height: 100vh">
+        <v-col sm="12" cols="12" align-self="center">
+          <h1>Registro</h1>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
-
 <script>
+import HeaderTop from './partials/header-top.vue'
+
 export default {
   name: 'Register',
+  components: {
+    HeaderTop
+  }
 }
 </script>
 
---
+++ src/components/partials/header-top.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-container fluid class="header-top">
+    <v-row>
+      <v-col cols="1" sm="1" align-self="center">
+        <v-icon color="white" @click="goBack">arrow_back</v-icon>
+      </v-col>
+      <v-col cols="11" sm="11" class="py-3">
+
+        <p>RED</p>
+        <p>FORTALECIMIENTO</p>
+        <p>EMPRESARIAL</p>
+      </v-col>
+    </v-row>
+
+  </v-container>
+</template>
+
+<script>
+import router from "@/router";
+
+export default {
+  data: () => {
+    return {}
+  },
+  mounted() {
+  },
+  methods: {
+    goBack() {
+      router.go(-1);
+    }
+  },
+  computed() {
+  },
+  components: {}
+}
+</script>
+
+<style scoped>
+.header-top {
+  background-color: #000;
+  padding: 1rem;
+  margin: 0;
+  border-bottom: 1px solid #0072F7;
+  color: #fff;
+}
+
+.header-top p {
+  padding: 0;
+  margin: 0;
+}
+</style>
